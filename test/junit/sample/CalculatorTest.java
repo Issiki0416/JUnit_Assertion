e5f6f99541d8b622d74929a6d6322cd0c527a408
tsuchimoto_yusuke <tsuchimoto_yusuke@comsq.com>
2024-04-07 12:33:38 +0900

    JUnitのfailメソッドを使った実装

---
+++ test/junit/sample/CalculatorTest.java
@@ -0,0 +1,27 @@
+package junit.sample;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.Test;
+
+public class CalculatorTest {
+	@Test
+	void testAdd() {
+		fail("まだ実装していない");
+	}
+	
+	@Test
+	void testSub() {
+		fail("まだ実装していない");
+	}
+	
+	@Test
+	void testMul() {
+		fail("まだ実装していない");
+	}
+	@Test
+	void testDiv() {
+		fail("まだ実装していない");
+	}
+
+}
